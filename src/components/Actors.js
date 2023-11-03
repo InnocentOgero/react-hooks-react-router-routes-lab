7e40ec680ad5cd9ca5bd683aadcf187dd1f9412a
InnocentOgero <innocent.ogero@student.moringaschool.com>
2023-11-03 12:27:10 +0300

    completed assignment

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -2,7 +2,23 @@ import React from "react";
 import { actors } from "../data";
 
 function Actors() {
-  return <div>{/*{code here}*/}</div>;
+  return <div>
+    <h1>Actors Page</h1>
+    {
+      actors.map(actor=>(
+        <div key={actor.name}>
+          <h2>Name:{actor.name}</h2>
+          <p>Movies:</p>
+          {
+            actor.movies.map(movie=>(
+              <p key={movie}>{movie}</p>
+            ))
+          }
+        </div>
+      ))
+
+    }
+    </div>;
 }
 
 export default Actors;
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,8 +1,36 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 
+const styleLinks={marginBottom:"10px"}
 function NavBar() {
-  return <div>{/*{code here}*/}</div>;
+  return (
+  <div style={{display:"flex",flexDirection:"column"}}>
+    <NavLink
+    key="home"
+    to="/"
+    style={styleLinks}
+    activestyle={{color:'blue'}}
+    >Home</NavLink>
+    <NavLink
+    key="movies"
+    to="/movies"
+    style={styleLinks}
+    activestyle={{color:'blue'}}
+    
+    >Movies</NavLink>
+    <NavLink
+    key="directors"
+    to="/directors"
+    style={styleLinks}
+    activestyle={{color:'blue'}}
+    >Directors</NavLink>
+    <NavLink
+    key="actors"
+    to="/actors"
+    style={styleLinks}
+    activestyle={{color:'blue'}}
+    >Actors</NavLink>
+  </div>);
 }
 
 export default NavBar;
